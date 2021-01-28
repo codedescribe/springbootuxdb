ea130a0b9455338f162646d12123c329c11f35b0
yangfan <maxfan123@qq.com>
2021-01-28 11:24:18 +0800

    Initial commit

---
+++ src/main/java/com/maxfan/springbootuxdb/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.maxfan.springbootuxdb;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringbootuxdbApplication.class);
+    }
+
+}
---
+++ src/main/java/com/maxfan/springbootuxdb/SpringbootuxdbApplication.java
@@ -0,0 +1,13 @@
+package com.maxfan.springbootuxdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootuxdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootuxdbApplication.class, args);
+    }
+
+}
